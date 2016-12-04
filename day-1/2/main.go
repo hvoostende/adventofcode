dfbe26a97db9e7702d8516d5ffd77fe5b9b216c4
hvoostende <hvoostende@gmail.com>
2016-12-04 21:09:27 +0100

    Day 1 part 2

--- day-1/2/main.go
+++ day-1/2/main.go
@@ -28,6 +28,11 @@ var directions = []string{
 
 func (d Direction) String() string { return directions[d] }
 
+var (
+	x, y      int
+	locations []string
+)
+
 func check(e error) {
 	if e != nil {
 		log.Fatalln(e)
@@ -38,33 +43,78 @@ func main() {
 	const input = "L2, L3, L3, L4, R1, R2, L3, R3, R3, L1, L3, R2, R3, L3, R4, R3, R3, L1, L4, R4, L2, R5, R1, L5, R1, R3, L5, R2, L2, R2, R1, L1, L3, L3, R4, R5, R4, L1, L189, L2, R2, L5, R5, R45, L3, R4, R77, L1, R1, R194, R2, L5, L3, L2, L1, R5, L3, L3, L5, L5, L5, R2, L1, L2, L3, R2, R5, R4, L2, R3, R5, L2, L2, R3, L3, L2, L1, L3, R5, R4, R3, R2, L1, R2, L5, R4, L5, L4, R4, L2, R5, L3, L2, R4, L1, L2, R2, R3, L2, L5, R1, R1, R3, R4, R1, R2, R4, R5, L3, L5, L3, L3, R5, R4, R1, L3, R1, L3, R3, R3, R3, L1, R3, R4, L5, L3, L1, L5, L4, R4, R1, L4, R3, R3, R5, R4, R3, R3, L1, L2, R1, L4, L4, L3, L4, L3, L5, R2, R4, L2"
 	instructions := strings.Split(input, ", ")
 
-	var x int
-	var y int
 	compas := north
 
 	for _, v := range instructions {
-		//fmt.Printf("Go %v, %v blocks\n", v[:1], v[1:])
+
 		newCompas, e := changeCompas(compas, v[:1])
-		compas = newCompas
 		check(e)
+
+		compas = newCompas
+
 		i, err := strconv.Atoi(v[1:])
 		check(err)
-		switch newCompas {
-		case north:
-			y += i
-		case east:
-			x += i
-		case south:
-			y -= i
-		case west:
-			x -= i
-		}
-		fmt.Println(newCompas)
-		fmt.Println("x:", x)
-		fmt.Println("y:", y)
-	}
 
-	fmt.Println("The shortest path to the destination is: ", x+y)
+		if walk(compas, i) {
+			break
+		}
+	}
+}
+
+func walk(compas Direction, j int) bool {
+	for i := 0; i < j; i++ {
+		switch compas {
+		case north:
+			y++
+			if visitedTwice(x, y) {
+				return true
+			}
+		case east:
+			x++
+			if visitedTwice(x, y) {
+				return true
+			}
+		case south:
+			y--
+			if visitedTwice(x, y) {
+				return true
+			}
+		case west:
+			x--
+			if visitedTwice(x, y) {
+				return true
+			}
+		}
+	}
+	return false
+
+}
+
+func visitedTwice(x, y int) bool {
+	sx := strconv.Itoa(x)
+	sy := strconv.Itoa(y)
+	if checkLocation("x"+sx+"y"+sy, locations) {
+		fmt.Println("the first location you visit twice is: ", "x"+sx+"y"+sy)
+		if x < 0 {
+			x = x * -1
+		}
+		if y < 0 {
+			y = y * -1
+		}
+		fmt.Printf("This is %v blocks away", x+y)
+		return true
+	}
+	locations = append(locations, "x"+sx+"y"+sy)
+	return false
+}
+
+func checkLocation(location string, locations []string) bool {
+	for i, _ := range locations {
+		if location == locations[i] {
+			return true
+		}
+	}
+	return false
 }
 
 func changeCompas(direction Direction, turn string) (Direction, error) {
@@ -96,43 +146,15 @@ func changeCompas(direction Direction, turn string) (Direction, error) {
 
 	}
 	return undefined, errors.New("changeCompas: wrong turn argument")
-
 }
 
 /*
---- Day 1: No Time for a Taxicab ---
+--- Part Two ---
+Then, you notice the instructions continue on the back of the Recruiting Document.
+Easter Bunny HQ is actually at the first location you visit twice.
 
-Santa's sleigh uses a very high-precision clock to guide its movements,
-and the clock's oscillator is regulated by stars.
-Unfortunately, the stars have been stolen... by the Easter Bunny.
-To save Christmas, Santa needs you to retrieve all fifty stars by December 25th.
+For example, if your instructions are R8, R4, R4, R8,
+the first location you visit twice is 4 blocks away, due East.
 
-Collect stars by solving puzzles.
-Two puzzles will be made available on each day in the advent calendar;
-the second puzzle is unlocked when you complete the first.
-Each puzzle grants one star.
-Good luck!
-
-You're airdropped near Easter Bunny Headquarters in a city somewhere.
-"Near", unfortunately, is as close as you can get -
-the instructions on the Easter Bunny Recruiting Document the Elves intercepted
-start here, and nobody had time to work them out further.
-
-The Document indicates that you should start at the given coordinates
-(where you just landed) and face North. Then, follow the provided sequence:
-either turn left (L) or right (R) 90 degrees, then walk forward the given number
-of blocks, ending at a new intersection.
-
-There's no time to follow such ridiculous instructions on foot, though, so you
-take a moment and work out the destination.
-Given that you can only walk on the street grid of the city,
-how far is the shortest path to the destination?
-
-For example:
-
-Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
-R2, R2, R2 leaves you 2 blocks due South of your starting position,
-which is 2 blocks away.
-R5, L5, R5, R3 leaves you 12 blocks away.
-How many blocks away is Easter Bunny HQ?
+How many blocks away is the first location you visit twice?
 */
